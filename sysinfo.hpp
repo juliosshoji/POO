3cfc02abb031c859c1ea162edaab8198d56cce11
juliozotokoba <j245708@dac.unicamp.br>
2024-05-29 22:11:40 -0300

    Adicionando sysinfo

---
+++ sysinfo.hpp
@@ -0,0 +1,33 @@
+/*
+Júlio Shoji Zotovici Kobayashi
+Class SysInfo
+June, 2024
+*/
+#ifndef SYSINFO_HPP_
+
+#define SYSINFO_HPP_
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class sysinfo{
+    private:
+        string universityName = "UNICAMP - Universidade Estadual de Campinas";
+        //Adicionar seus nome aqui
+        vector<string> creatorsNames {"Júlio Shoji Zotovici Kobayashi"}; 
+        string professorName = "Professor Doutor Andre F. Angelis";
+        string instituteName = "FT - Faculdade de Tecnologia";
+        string contact = "j245708@dac.unicamp.br";
+        string campusAddres = "R. Paschoal Marmo, 1888 - Jardim Nova Italia, Limeira - SP, 13484-332";
+    public:
+        string getUniversity();
+        vector<string> getCreators();
+        string professorName();
+        string getInstitute();
+        string getContact();
+        string getCampus();
+};
+
+#endif
