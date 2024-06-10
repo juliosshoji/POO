f6aa780acb3a05010e23831930de7685c40c1984
juliozotokoba <j245708@dac.unicamp.br>
2024-06-10 15:53:53 -0300

    Debuging

--- LocalSystem/SeriesDAO.cpp
+++ LocalSystem/SeriesDAO.cpp
@@ -33,12 +33,11 @@ vector<Series> SeriesDAO::getSeriesOrderByTitle(){
    vector<Series>SeriesByTitle;
     try
     {   
-        size_t indexPrimary = 1;
         string s1, s2;
         SeriesByTitle = this->serverConn->getSeries();
         Series auxiliar = SeriesByTitle.at(0);
-        for(size_t indexSecondery = 0; indexSecondery < SeriesByTitle.size(); indexSecondery++){
-            for(Series serie : SeriesByTitle){
+        for(size_t indexSecondery = 1; indexSecondery < SeriesByTitle.size(); indexSecondery++){
+            for(size_t indexPrimary = 1; indexPrimary <= indexSecondery; indexPrimary++){
                 s1 = SeriesByTitle[indexPrimary].getSeries_name();
                 s2 = auxiliar.getSeries_name();
                 if(s1[0] < s2[0]){
