38eca57431e43e9dcbd66c55868313a4dbac2141
juliozotokoba <j245708@dac.unicamp.br>
2024-06-10 11:25:36 -0300

    Corrigindo erros

--- LocalSystem/SeriesDAO.cpp
+++ LocalSystem/SeriesDAO.cpp
@@ -19,7 +19,7 @@ Series SeriesDAO::getSeriesbyID(int internal_id){
         size_t index = 0;
         while(seriesIterator != serieList.end()){
             if((*seriesIterator)->getInternal_id() == internal_id){
-                RequestedSeries = serieList[index];
+                return serieList[index];
             }
             index++;
             seriesIterator++;
